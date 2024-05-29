25f0b2252a6314024c96124a6c5d30a85fa8bae6
Kacper <kacperkukula97@gmail.com>
2024-05-29 22:48:54 +0200

    Added authorization service and JWT

---
+++ src/main/java/com/carrental/security/CustomUserDetailsService.java
@@ -0,0 +1,17 @@
+package com.carrental.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
