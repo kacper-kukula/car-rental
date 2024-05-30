fa7d18287fd2dd8cbd91eb57bbb13b4e441ccbac
Kacper <kacperkukula97@gmail.com>
2024-05-30 21:37:33 +0200

    Added password validation, mock users, global exception handler

--- src/main/java/com/carrental/dto/user/UserRegistrationRequestDto.java
+++ src/main/java/com/carrental/dto/user/UserRegistrationRequestDto.java
@@ -1,9 +1,15 @@
 package com.carrental.dto.user;
 
+import com.carrental.security.validator.FieldMatch;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
 
+@FieldMatch(
+        first = "password",
+        second = "repeatPassword",
+        message = "The passwords must be identical"
+)
 public record UserRegistrationRequestDto(
         @NotBlank
         @Email
